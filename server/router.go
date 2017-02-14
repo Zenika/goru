109b51b6ae2ddf85460f037ae6a20d950480045d
nlepage <nicolas.lepage@zenika.com>
2017-02-14 12:37:09 +0100

    :sparkles: Add ability to configure context path

--- server/router.go
+++ server/router.go
@@ -2,21 +2,28 @@ package server // import "github.com/Zenika/goru/server"
 
 import (
 	"net/http"
+	"path"
 	"strconv"
 
 	"github.com/pkg/errors"
 
 	"github.com/husobee/vestigo"
+	"github.com/spf13/viper"
 )
 
 func StartRouter(port int) error {
 	router := vestigo.NewRouter()
 
-	router.Post("/api/document/:file/edit", postEditeurHandler)
-	router.Put("/api/document/:file/content", putDocumentHandler)
-	router.Get("/api/document/:file/content", getDocumentHandler)
+	contextPath := viper.GetString("server.contextPath")
+	if !path.IsAbs(contextPath) {
+		return errors.New("Context path must an absolute path")
+	}
 
-	println("Starting server on port", port)
+	router.Post(path.Join(contextPath, "document/:file/edit"), postEditeurHandler)
+	router.Put(path.Join(contextPath, "document/:file/content"), putDocumentHandler)
+	router.Get(path.Join(contextPath, "document/:file/content"), getDocumentHandler)
+
+	println("Starting server on port", port, "and context path", contextPath)
 
 	if err := http.ListenAndServe(":"+strconv.Itoa(port), router); err != nil {
 		return errors.Wrap(err, "Could not start HTTP server")
