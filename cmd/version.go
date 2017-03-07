6434ea3637605ec02d25597a9abcf1771fed506f
nlepage <nicolas.lepage@zenika.com>
2017-03-07 21:14:22 +0100

    Add a version command

---
+++ cmd/version.go
@@ -0,0 +1,19 @@
+package cmd // import "github.com/Zenika/goru/cmd"
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/Zenika/goru/version"
+	"github.com/spf13/cobra"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Show version",
+	Run: func(cmd *cobra.Command, args []string) {
+		log.Infof("Version: %s", version.Version)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(versionCmd)
+}
